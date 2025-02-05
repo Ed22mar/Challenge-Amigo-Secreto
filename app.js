25755bb5f9fd7539df207f2140733d5707cd8f8d
Marcos,Edgar <68207408+Ed22mar@users.noreply.github.com>
2025-02-06 00:51:24 +0800

    Criar as função de sortear

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
 // Array de amigos
 let amigos= [];
 
@@ -11,17 +12,26 @@ function adicionarAmigo(){
     }else{
         amigos.push(nomeDeAmigos);
         limparCampo();
-        listarAmigos();
-        
-    }
-    
+        listarAmigos();    
+    }   
+}
+
+// Função para fazer o sorteio de amigos
+function sortearAmigo(){
+    if(amigos.length == 0){
+        alert('Adiciona amigos a lista para serem sorteados!');
+    }else{
+        let sorteio = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[sorteio];
+        let resultado = document.getElementById("resultado");
+        resultado.innerHTML = amigoSorteado;
+    } 
 }
 
 // Função para adicionar amigos a lista
 function listarAmigos(){
     let lista =document.getElementById("listaAmigos");
     lista.innerHTML = "";
-
     for (let i = 0; i < amigos.length; i++) {
         let li = document.createElement("li");
             li.textContent = amigos[i];
